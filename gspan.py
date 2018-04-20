f49d14ee4a89f7aa12252f002cef99538fe711e1
Michael Gribskov <mgribsko@gmail.com>
2018-04-20 06:52:38 -0400

    gspan - graph_normalize passes test.  will add more test output

--- gspan.py
+++ gspan.py
@@ -1,4 +1,8 @@
-class gspan:
+import sys
+import copy
+
+
+class Gspan:
     """=============================================================================================
     Yan and Han algorithm for canonical graph labeling
     for now, assume a graph list of edges [v0, v1, e] where v0 and v1 are graph labels (arbitrary
@@ -20,6 +24,10 @@ class gspan:
             push equivalent forward extensions on unexplored
             
     stack has G, d2g and ?
+
+    much better
+    all you need to save is g2d list and row of edgelist
+    sorted edgelist is the dfs
             
     Michael Gribskov     20 April 2018
    
@@ -29,11 +37,42 @@ class gspan:
         """-----------------------------------------------------------------------------------------
         gspan constructor
         -----------------------------------------------------------------------------------------"""
-        self.g = []
+        self.graph = None
+        self.map = None
         self.mindfs = []
-        self.g2d = []
-        self.d2g = []
+        self.g2d = None
+        self.d2g = None
         self.unexplored = []
+
+    def graph_normalize(self):
+        """-----------------------------------------------------------------------------------------
+        Count the number of vertices and, if necessary, renumber so they begin at zero
+        initialize d2g and g2d to be the correct size
+        :return: int, number of vertices
+        -----------------------------------------------------------------------------------------"""
+        if self.graph is None:
+            sys.stderr.write('Gspan.graph_normalize - graph is undefined\n')
+
+        # list of vertices, index in v gives the normalized index
+        v = []
+        for edge in self.graph:
+            for i in range(0, 2):
+                if edge[i] not in v:
+                    v.append(edge[i])
+
+        # convert edge numbers
+        for edge in self.graph:
+            for i in range(0, 2):
+                edge[i] = v.index(edge[i])
+
+        self.vnum = len(v)
+        self.map = v
+
+        # initialize d2g and g2d
+        self.d2g = [None for i in range(0, self.vnum)]
+        self.g2d = [None for i in range(0, self.vnum)]
+
+        return self.vnum
 
 
 # ==================================================================================================
@@ -52,5 +91,17 @@ if __name__ == '__main__':
     graphset = [[[0, 1, i], [1, 2, i], [2, 0, j]],
                 [[0, 1, i], [0, 2, j], [1, 2, j]],
                 [[0, 1, j], [0, 2, j], [1, 2, j]]]
+
+    gspan = Gspan()
+
+    # graph normalization crate an unnormalized graph by doubling the vertex numbers
+    print('Graph normalization')
+    g = copy.deepcopy(graphset[1])
+    for edge in g:
+        for i in range(0, 2):
+            edge[i] *= 2
+
+    gspan.graph = g
+    gspan.graph_normalize()
 
     exit(0)
