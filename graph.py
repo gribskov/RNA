2699c6f95eb04da6427c16bd16a736d2cef1321e
Michael Gribskov <mgribsko@gmail.com>
2018-04-25 19:24:22 -0400

    graph - removed Xios class.  These functions are now in RNAstructure.  possible is need for topologyEnumerate()

--- graph.py
+++ graph.py
@@ -223,6 +223,34 @@ class RNAGraph:
 # --------------------------------------------------------------------------------------------------
 # Non-object functions
 # --------------------------------------------------------------------------------------------------
+def possible(s):
+    """---------------------------------------------------------------------------------------------
+    return a list of the possible next stems.  Two kinds of stems can be added
+    1) stems with use zero can be added in numerical order
+    2) stems that have use == 1 can be added, but only if the the count of open stems would be > 0
+
+    :param s: stem usage list
+    :return: list of possible elements to add
+    ---------------------------------------------------------------------------------------------"""
+    possible = []
+    candidate = []
+    first = True
+    open = 0
+    all = 0
+    for i in range(len(s)):
+        all += 2 - s[i]
+        if s[i] == 0 and first:
+            first = False
+            possible.append(i)
+        elif s[i] == 1:
+            candidate.append(i)
+            open += 1
+
+    if open > 1 or all == 1:
+        possible += candidate
+
+    return possible
+
 
 def enumerateRNATopology(n):
     """---------------------------------------------------------------------------------------------
