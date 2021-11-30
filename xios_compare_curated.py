cb298e2edd36a65e5884a88e6d46c1d49b5625be
Michael Gribskov <mgribsko@gmail.com>
2021-11-30 15:05:40 -0500

    xios_compare_curated - summary of stems in each file

--- xios_compare_curated.py
+++ xios_compare_curated.py
@@ -1,8 +1,23 @@
 """=================================================================================================
 compare xios file to xios of a curated file and evaluate overlap
+
 ================================================================================================="""
 import sys
 from topology import Topology
+
+def summary( xios, title ):
+    '''---------------------------------------------------------------------------------------------
+    Summarize the stems in the XIOS structure
+    :param xios: Topology object
+    : param title: string, describes file in summary
+    :return: True
+    ---------------------------------------------------------------------------------------------'''
+    print( f'\n{title} XIOS file of {xios.sequence_id}')
+    print( f'\tstems: {len(xios.stem_list)}')
+    for s in xios.stem_list:
+        print( f'\t{s["name"]}\t{s["left_begin"]}\t{s["left_end"]}\t{s["right_begin"]}\t{s["right_end"]}')
+
+    return True
 
 if __name__ == '__main__':
 
@@ -12,10 +27,12 @@ if __name__ == '__main__':
     # read reference xios (gold standard)
     ref = Topology()
     ref.XIOSread(refname)
+    summary(ref, 'Curated')
 
-    # read subject xios
+    # read subject xios (test structure to compare)
     subject = Topology()
     subject.XIOSread(subjectname)
+    summary(subject, 'Test')
 
     exit(0)
 
