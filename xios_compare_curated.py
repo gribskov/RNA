82b5e170eec62c93fe7cae46713fc2ecd2cac91f
Michael Gribskov <mgribsko@gmail.com>
2021-12-01 08:21:22 -0500

    xios_compare_curated - overlap completed, and tested

--- xios_compare_curated.py
+++ xios_compare_curated.py
@@ -21,60 +21,94 @@ def summary(xios, title):
 
     return True
 
-
-def overlap(x1, x2, p1, p2):
+def check_left(x1, x2, p1, p2):
     '''---------------------------------------------------------------------------------------------
-    return overlap on per stem (True/False) and per base (int) basis
-    advance stem position p1 or p2 as needed
+    check if the left half stem of structure 1 overlaps a left half stem in structure 2, starting
+    at stem p1 and p2 in structure 1 and 2, respectively.
+    Stems in structures are sorted by left_begin
 
-    each stem in x1.stemlist and x2.stemlist is a dict of
-        {'name', 'center', 'left_begin', 'left_end', 'right_begin', 'right_end', 'left_vienna', 
-        'right_vienna' }
-
-    :param x1: Topology object, XIOS structure 1
-    :param x2: Topology object, XIOS structure 2
-    :param p1: int, current stem position in structure 1
-    :param p2: int, current stem position in structure 2
-    :return: boolean, int - stem overlaps, number of overlapping bases
+    :param x1: Topology object, structure 1
+    :param x2: Topology object, structure 2
+    :param p1: stem of interest in structure 1
+    :param p2: starting stem in structure 2
+    :return: list of int, stems in structure 2 whos left half-stems overlap p1's
     ---------------------------------------------------------------------------------------------'''
-    stem = False
-    base = 0
+    overlap_set = []
+    stem1 = x1.stem_list[p1]
+    pos2 = p2
+    while True:
+        stem2 = x2.stem_list[pos2]
+        if stem2['left_begin'] > stem1['left_end']:
+            # end condition for while loop, stem2 is to the right of stem1
+            break
 
-    sl_1 = x1.stem_list
-    sl_2 = x2.stem_list
-    if sl_1[p1]['left_begin'] > sl_2[p2]['left_end']:
-        print(f'{p1} > {p2}')
-        p2 += 1
-        return (p1, p2, stem, base)
+        if stem2['left_end'] < stem1['left_begin']:
+            # stem2 is to the left of stem1
+            pos2 += 1
+            continue
 
-    if sl_1[p1]['left_end'] < sl_2[p2]['left_begin']:
-        print(f'{p1} < {p2}')
-        p1 += 1
-        return (p1, p2, stem, base)
+        # if stem 2 is not to the left or right it overlaps
+        overlap_set.append(pos2)
+        print('\tleft: stem1:{} overlaps stem2:{}'.format(p1, pos2))
+        pos2 += 1
 
-    # only reach her if left half stem overlaps, now check the right side
+    return overlap_set
 
-    if sl_1[p1]['right_begin'] > sl_2[p2]['right_end']:
-        print(f'{p1} > {p2}')
-        p2 += 1
-        return (p1, p2, stem, base)
 
-    if sl_1[p1]['right_end'] < sl_2[p2]['right_begin']:
-        print(f'{p1} < {p2}')
-        p2 += 1
-        return (p1, p2, stem, base)
+def check_right(x1, x2, p1, overlap):
+    '''---------------------------------------------------------------------------------------------
+    overlap contains a list of stems in structure 2 that overlap stem p1 in structure 1 on the
+    left. Check each one to see if it overlaps on the right
 
-    # both sides overlap
-    print('{}|{}\t{}\t{}\t{}\t\t{}|{}\t{}\t{}\t{}'.format(
-        p1, sl_1[p1]['left_begin'], sl_1[p1]['left_end'],
-        sl_1[p1]['right_begin'], sl_1[p1]['right_end'],
-        p2, sl_2[p2]['left_begin'], sl_2[p2]['left_end'],
-        sl_2[p2]['right_begin'], sl_2[p2]['right_end']
-        ))
-    stem = True
-    p2 += 1
+    :param x1: Topology object, structure 1
+    :param x2:Topology object, structure 2
+    :param p1: int, stem of interest in structure 1
+    :param overlap: list of int, overlapping left half stems for stem p1 in structure 1
+    :return: list of int, stems in structure 2 that overlap with both left and right halfstems of p1
+    ---------------------------------------------------------------------------------------------'''
+    both = []
+    stem1 = x1.stem_list[p1]
+    for p2 in overlap:
+        stem2 = x2.stem_list[p2]
 
-    return p1, p2, stem, base
+        if stem2['right_begin'] > stem1['right_end'] or \
+                stem2['right_end'] < stem1['right_begin']:
+            # no overlap
+            continue
+
+        print('\tright: stem1:{} overlaps stem2:{}'.format(p1, p2))
+        both.append(p2)
+
+    return both
+
+
+def overlap2(x1, x2):
+    '''---------------------------------------------------------------------------------------------
+    compare the stems in structure 1 and structure 2 and identify the overlapping stems
+    :param x1: Topology object, structure 1
+    :param x2: Topology object, structure 2
+    :return:
+    ---------------------------------------------------------------------------------------------'''
+    p2 = 0
+    left = []
+    for p1 in range(len(x1.stem_list)):
+        p2 = 0
+        left = check_left(x1, x2, p1, p2)
+        if left:
+            # one or more overlapping left half-stems
+            both = check_right(x1, x2, p1, left)
+        else:
+            continue
+
+        if both:
+            for p2 in both:
+                s1 = x1.stem_list[p1]
+                s2 = x2.stem_list[p2]
+                print('overlap x1:{} x2:{}  -  {}\t{}\t{}\t{}  -  {}\t{}\t{}\t{}'.format(
+                    p1, p2,
+                    s1['left_begin'], s1['left_end'], s1['right_begin'], s1['right_end'],
+                    s2['left_begin'], s2['left_end'], s2['right_begin'], s2['right_end']
+                    ))
 
 
 if __name__ == '__main__':
@@ -91,9 +125,6 @@ if __name__ == '__main__':
     subject.XIOSread(subjectname)
     summary(subject, 'Test')
 
-    p1 = 0
-    p2 = 0
-    while p1 < len(ref.stem_list):
-        p1, p2, stem, base = overlap(ref, subject, p1, p2)
+    overlap2(ref, subject)
 
     exit(0)
