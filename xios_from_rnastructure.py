22a64b66a5bf56dbe1e715d3a7e793ae4726f4f0
Michael Gribskov <mgribsko@gmail.com>
2021-12-09 12:01:31 -0500

    xios_from_rnastructure - command line arguments

---
+++ xios_from_rnastructure.py
@@ -0,0 +1,162 @@
+"""=================================================================================================
+Pipeline for generating XIOS structures using the program fold in the RNAStructure package.
+
+Steps:
+    fold program (RNAstructure) - calculate suboptimal structures --> ct file
+    mergestems.pl -- merge multiple suboptimal structures --> XIOS file
+
+================================================================================================="""
+import argparse
+
+
+def formatter(prog):
+    """---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    Set up formatting for help
+    :param prog:
+    :return: argparse formatter class
+    ---------------------------------------------------------------------------------------------"""
+    return argparse.HelpFormatter(prog, max_help_position=50, width=100)
+
+
+def options():
+    """---------------------------------------------------------------------------------------------
+    Command line options parsed with argparse
+
+    :return: argparse Namespace (similar to a dictionary)
+    ---------------------------------------------------------------------------------------------"""
+    commandline = argparse.ArgumentParser(
+        description='Create XIOS from FastaA sequence using the RNAstructure fold program',
+        formatter_class=formatter)
+    commandline.add_argument('-i', '--indir',
+                             help='Directory containing FastA files',
+                             default='/')
+    commandline.add_argument('-s', '--suffix',
+                             help='Suffix for input FastA files (%(default)s)',
+                             default='.fa')
+    commandline.add_argument('-p', '--percent',
+                             help='Fold: percent cutoff for suboptimal structures (%(default)s)',
+                             default=4)
+    commandline.add_argument('-m', '--maximum',
+                             help='Fold: maximum number of suboptimal structures (%(default)s)',
+                             default=20)
+    commandline.add_argument('-w', '--window',
+                             help='Fold: window for suboptimal structures (%(default)s)',
+                             default=4)
+    commandline.add_argument('-d', '--ddG',
+                             help='Mergestems: delta deltaG limit for supoptimal structures (%(default)s)',
+                             default=5)
+    commandline.add_argument('-c', '--mergecase',
+                             help='Mergestems: merging cases (rules) for combining suboptimal '
+                                  'structures (%(default)s)',
+                             default='12345')
+
+    # process command line arguments
+    return commandline.parse_args()
+
+
+def runfold(percent, maxstruct, window, fasta, ct):
+    """---------------------------------------------------------------------------------------------
+    USAGE: Fold <sequence file> <CT file> [options]
+    All flags are case-insensitive, and grouping of flags is not allowed.
+
+    ==== Required Parameters ====
+    <sequence file>
+        The name of a file containing an input sequence. Acceptable formats include
+        SEQ, FASTA and raw-sequence plain-text files.
+        If the name is a hyphen (-), the file will be read from standard input
+        (STDIN).
+
+    <CT file>
+        The name of a CT file to which output will be written. If the --bracket
+        flag is present, output will be written as a dot-bracket file.
+        If the file name is a hyphen (-), the structure will be written to standard
+        output (STDOUT) instead of a file.
+
+    ==== Selected Options ====
+
+    -l -L --loop
+        Specify a maximum internal/bulge loop size.
+        Default is 30 unpaired numcleotides.
+
+    -m -M --maximum
+        Specify a maximum number of structures.
+        Default is 20 structures.
+
+    --name
+        Specify a name for the sequence. This will override the name in the
+        sequence file.
+
+    -p -P --percent
+        Specify a maximum percent energy difference.
+        Default is 10 percent (specified as 10, not 0.1).
+
+    -s -S --save
+        Specify the name of a save file, needed for dotplots and refolding.
+        Default is not to generate a save file.
+
+    -t -T --temperature
+        Specify the temperature at which calculation takes place in Kelvin.
+        Default is 310.15 K, which is 37 degrees C.
+    -w -W --window
+        Specify a window size.
+        Default is determined by the length of the sequence.
+
+    --warnings --warn
+        Set the behavior for non-critical warnings (e.g. related to invalid
+        nucleotide positions or duplicate data points in SHAPE data). Valid values
+        are:
+          * on  -- Warnings are written to standard output. (default)
+          * err -- Warnings are sent to STDERR. This can be used in automated scripts
+            etc to detect problems.
+          * off -- Do not display warnings at all (not recommended).
+
+    :param percent: int, maximum percent ddG for suboptimal structures
+    :param maxstruct: int, maximum number of suoptimal structures
+    :param window: int, window size for filtering suboptimal structures
+    :param fasta: string, readable fasta file
+    :param ct: string, writable ct file
+    :return:
+    ---------------------------------------------------------------------------------------------"""
+
+    return
+
+
+def runmergestems(mergecases, ddG, ct):
+    """---------------------------------------------------------------------------------------------
+    mergestem.pl [-c <mergecases>] [-g <ddG>] [l <int>] [-p <plot_file>]  [-q] [-m <curated_file>] 
+            <CT_file>
+    
+    Mergestem compares that multiple structures in the CT file and convertss them to a single XIOS 
+    structure.
+    
+    :param mergecases: string, list of mergecases to apply
+    :param ddG: maximum delta delta G for suboptimal structures
+    :param ct: string, readable ct file
+    :return: 
+    ---------------------------------------------------------------------------------------------"""
+
+    return
+
+
+# --------------------------------------------------------------------------------------------------
+# main program
+# --------------------------------------------------------------------------------------------------
+if __name__ == '__main__':
+    # code locations
+    rnastructure = "/scratch/bell/mgribsko/rna/RNAstructure/exe"
+    perl_src = "/depot/mgribsko/rna/RNA/perl_src"
+
+    print(f'RNAstructure executables {rnastructure}')
+    print(f'Mergestem executable {perl_src}')
+
+    # command line options
+    args = options()
+
+
+    # run fold
+
+    # runfold(p, m, args.fasta)
+
+    # run mergestems
+
+    exit(0)
