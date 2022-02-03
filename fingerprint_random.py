aa72143f03918d19ce8f1c1549a7156562d367d5
Michael Gribskov <mgribsko@gmail.com>
2022-02-03 15:09:55 -0500

    fingerprint_random.py - added back command line arguments, added single letter options, added options quiet and limit, tested and working

--- fingerprint_random.py
+++ fingerprint_random.py
@@ -13,51 +13,63 @@ from fingerprint import Fingerprint
 
 def process_command_line():
     """---------------------------------------------------------------------------------------------
-    usage: fingerprint_random.py [-h] [--motifdb_file MOTIFDB_FILE] [--rna_file RNA_FILE] [--fpt_file FPT_FILE]
-                                 [--subgraphsize SUBGRAPHSIZE] [--coverage COVERAGE] [--noparent]
+    usage: fingerprint_random.py [-h] [-m MOTIFDB] [-r RNA] [-f FPT] [-s SUBGRAPHSIZE] [-c COVERAGE] [-l LIMIT] [-n] [-q]
 
     Calculate an XIOS fingerprint from a XIOS XML file by random sampling
 
     optional arguments:
-      -h, --help                   show this help message and exit
-      --motifdb_file MOTIFDB_FILE  RNA motif file
-      --rna_file RNA_FILE          RNA topology in XIOS XML format
-      --fpt_file FPT_FILE          Fingerprint output file (default=STDOUT)
-      --subgraphsize SUBGRAPHSIZE  Size subgraph to sample (default=6)
-      --coverage COVERAGE          Minimum coverage for sampled graphs (default=3)
-      --noparent                   Include parent graphs in fingerprint (default=False)
+      -h, --help                            show this help message and exit
+      -m MOTIFDB, --motifdb MOTIFDB         RNA motif file
+      -r RNA, --rna RNA                     RNA topology in XIOS XML format
+      -f FPT, --fpt FPT                     Fingerprint output file (default=STDOUT)
+      -s SUBGRAPHSIZE, --subgraphsize SUBGRAPHSIZE
+                                            Size subgraph to sample (default=6)
+      -c COVERAGE, --coverage COVERAGE      Minimum coverage for sampled graphs (default=3)
+      -l LIMIT, --limit LIMIT               Maximum number of random graphs to sample (default=10000)
+      -n, --noparent                        Do not include parent graphs in fingerprint (default=False)
+      -q, --quiet                           Minimal output on stdout (default=False)
+
 
     :return:
     ---------------------------------------------------------------------------------------------"""
     default_subgraphsize = 6
     default_coverage = 3
+    default_sampling_limit = 10000
 
     cl = argparse.ArgumentParser(
         # argument_default=argparse.SUPPRESS,
         # formatter_class=lambda prog:argparse.ArgumentDefaultsHelpFormatter(prog,width=200,max_help_position=40),
         description='Calculate an XIOS fingerprint from a XIOS XML file by random sampling',
-        formatter_class=lambda prog: argparse.HelpFormatter(prog,width=120,max_help_position=40)
-    )
-    cl.add_argument('--motifdb',
-                    help='RNA motif file',
-                    type=argparse.FileType('r'))
-    cl.add_argument('-r','--rna',
+        formatter_class=lambda prog: argparse.HelpFormatter(prog, width=120, max_help_position=40)
+        )
+    cl.add_argument('-m', '--motifdb',
+                    help='pickled RNA motif file (default=%(default)s)',
+                    type=argparse.FileType('rb'),
+                    default='2to7stem.mdb.pkl')
+    cl.add_argument('-r', '--rna',
                     help='RNA topology in XIOS XML format',
                     type=argparse.FileType('r'))
-    cl.add_argument('--fpt',
+    cl.add_argument('-f', '--fpt',
                     help='Fingerprint output file (default=STDOUT)',
                     type=argparse.FileType('w'),
                     default=sys.stdout)
-    cl.add_argument('--subgraphsize',
+    cl.add_argument('-s', '--subgraphsize',
                     help='Size subgraph to sample (default=%(default)s)',
                     type=int,
-                    default = default_subgraphsize)
-    cl.add_argument('--coverage',
+                    default=default_subgraphsize)
+    cl.add_argument('-c', '--coverage',
                     help='Minimum coverage for sampled graphs (default=%(default)s)',
                     type=int,
                     default=default_coverage)
-    cl.add_argument('--noparent',
-                    help='Include parent graphs in fingerprint (default=%(default)s)',
+    cl.add_argument('-l', '--limit',
+                    help='Maximum number of random graphs to sample (default=%(default)s)',
+                    type=int,
+                    default=default_sampling_limit)
+    cl.add_argument('-n', '--noparent',
+                    help='Do not include parent graphs in fingerprint (default=%(default)s)',
+                    action='store_true')
+    cl.add_argument('-q', '--quiet',
+                    help='Minimal output on stdout (default=%(default)s)',
                     action='store_true')
 
     return cl.parse_args()
@@ -70,41 +82,34 @@ daytime = datetime.datetime.now()
 runstart = daytime.strftime('%Y-%m-%d %H:%M:%S')
 opt = process_command_line()
 
-# for testing
-# opt.motifdb_file = 'data/5stem.list.txt'
-# opt.rna_file = 'data/rnasep_a1.Buchnera_APS.xios'
-# opt.fpt_file = 'data/test2.fpt'
-print('fingerprint_random - Sample XIOS fingerprint from RNA topology {}'.format(runstart))
-print('\tRNA structure: {}'.format(opt.rna.name))
-# print('\tMotif database: {}'.format(opt.motifdb.name))
-print('\tFingerprint: {}'.format(opt.fpt.name))
-print('\tSubgraph size: {}'.format(opt.subgraphsize))
-print('\tCoverage (minimum): {}'.format(opt.coverage))
-print('\tOmit parents: {}'.format(opt.noparent))
+if opt.quiet:
+    print(f'fingerprint_random: {daytime} : {opt.rna.name} : {opt.fpt.name}', end=' ')
+else:
+    print('fingerprint_random - Sample XIOS fingerprint from RNA topology {}'.format(runstart))
+    print('\tRNA structure: {}'.format(opt.rna.name))
+    print('\tMotif database: {}'.format(opt.motifdb.name))
+    print('\tFingerprint: {}'.format(opt.fpt.name))
+    print('\tSubgraph size: {}'.format(opt.subgraphsize))
+    print('\tCoverage (minimum): {}'.format(opt.coverage))
+    print('\tOmit parents: {}'.format(opt.noparent))
 
 # read in the RNA structure
 rna = Topology(xml=opt.rna)
 # print(rna.format_edge_list())
 
 # this is an unweighted sampling strategy.  Others were tried, sampling:
-#   inversely proportional to number of times previously sampled
-#       scaled by 1/n and 1/rank
+#   inversely proportional to number of times previously sampled scaled by 1/n and 1/rank
 #   proportional to number of neighbors
 #   inversely proportional to number of neighbors
 
 fingerprint = Fingerprint()
 fingerprint.information['Date'] = runstart
 fingerprint.information['File'] = opt.fpt.name
-# fingerprint.information['Motif database'] = opt.motifdb.name
-# fingerprint.information['Motif database checksum'] = motif.information['checksum']
-# fingerprint.information['Motif database description'] = motif.information['name']
+fingerprint.information['Motif database'] = opt.motifdb.name
 fingerprint.information['RNA structure'] = opt.rna.name
 
-subgraph = opt.subgraphsize
-count_threshold = opt.coverage
-
 # sample the first subgraph to have a motif with minimum occurence
-xios = rna.sample_xios(subgraph)
+xios = rna.sample_xios(opt.subgraphsize)
 gspan = Gspan(graph=xios)
 dfs = gspan.minDFS().human_encode()
 fingerprint.add(dfs)
@@ -114,12 +119,12 @@ while True:
     # sample until the lowest count motif is above the opt.coverage count_threshold.  You only have
     # to recheck the minimum count when your current minimum graph passes the threshold (finding
     # minimum is expensive)
-    xios = rna.sample_xios(subgraph)
+    xios = rna.sample_xios(opt.subgraphsize)
 
     gspan = Gspan(graph=xios)
     dfs = gspan.minDFS().human_encode()
     # print(dfs)
-    if not fingerprint.count % 10000:
+    if not opt.quiet and not fingerprint.count % 10000:
         print(fingerprint.count, dfs)
         fingerprint.writeYAML(sys.stderr)
     fingerprint.add(dfs)
@@ -128,23 +133,35 @@ while True:
         # if the new dfs is the one with the lowest count, update the lowest count
         minmotif = fingerprint.minkey()
         mincount = fingerprint.mincount()
-        print('{}\t{}\t{}'.format(fingerprint.count, fingerprint.n, fingerprint.mincount()))
-        if mincount >= count_threshold or fingerprint.count>sample_threshold:
+        # print('{}\t{}\t{}'.format(fingerprint.count, fingerprint.n, fingerprint.mincount()))
+        if mincount >= opt.coverage or fingerprint.count > opt.limit:
             break
 
-print('Simple fingerprint: {}\t{}\t{}'.format(fingerprint.count, fingerprint.n,
-                                              fingerprint.mincount()))
+n_initial = fingerprint.n
+if opt.noparent:
+    if opt.quiet:
+        print(f'fpt: {fingerprint.n} : {fingerprint.count}')
+    else:
+        print('Simple fingerprint: {}\t{}\t{}'.format(fingerprint.count, fingerprint.n, fingerprint.mincount()))
+
 
 # to include parent, you must read a motif database
 if not opt.noparent:
-# read in the motif database and RNA structure
-    # TODO change pickled file to opt.motifdb when done testing
-    motif = MotifDB.unpickle('2to7stem.mdb.pkl')
+    # read in the motif database and RNA structure
+    motif = MotifDB.unpickle(opt.motifdb)
+    fingerprint.information['Motif database checksum'] = motif.information['checksum']
+    fingerprint.information['Motif database description'] = motif.information['name']
     fingerprint.add_parents(motif)
-    print('Extended fingerprint: {}\t{}\t{}'.format(fingerprint.count, fingerprint.n,
-                                                    fingerprint.mincount()))
+    if opt.quiet:
+        print(f'xpt: {fingerprint.n} : {fingerprint.count}')
+    else:
+        print('\nExtended fingerprint: {}\t{}\t{}'.format(fingerprint.count, fingerprint.n, fingerprint.mincount()))
+        print(f'\t{n_initial} simple fingerprints extended to {fingerprint.n}')
 
 # print(fingerprint.toYAML())
+if not opt.quiet:
+    print(f"\twriting to {motif.information['file']}")
+print()
 fingerprint.writeYAML(opt.fpt)
 
 daytime = datetime.datetime.now()
