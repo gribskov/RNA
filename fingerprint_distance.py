7a4342171775f5567a159c96887f5ec2b4e6cb61
mgribsko <mgribskov@gmail.com>
2022-12-09 11:27:27 -0500

    fingerprint_distance modified command line options. default output type is now .dist, both input and output directory can be specified

--- fingerprint_distance.py
+++ fingerprint_distance.py
@@ -1,49 +1,61 @@
 """=================================================================================================
 Calculate the distance between fingerprints.
-Assume fingerprint are all in the same directory
-Calculate the jaccard distance between all pairs
+Assume fingerprints are all in the same directory
+Calculate the jaccard similarity and Cray-Curtis distance between all pairs
 
 Michael Gribskov     04 February 2022
 ================================================================================================="""
 import glob
 from os.path import basename
 from fingerprint import Fingerprint, FingerprintSet
+import datetime
+
+
 def process_command_line():
     """---------------------------------------------------------------------------------------------
+    read command line options and return as a Namespace object. The Namespace object behaves much as
+    a dictionary, and can be converted to a dictionary using the vars() method
 
+    :return: Namespace object
     ---------------------------------------------------------------------------------------------"""
+    import argparse
+
     cl = argparse.ArgumentParser(
         description='Calculate distances between fingerprints',
         formatter_class=lambda prog: argparse.HelpFormatter(prog, width=120, max_help_position=40)
     )
-    cl.add_argument('-m', '--dir',
+    cl.add_argument('-m', '--indir',
                     help='Directory with fingerprint files (default=%(default)s)',
                     default='../data/fpt')
-    cl.add_argument('-r', '--suffix ',
-                    help='Suffix for fingertprint files  (default=%(default)s',
-                    tdefault='.fpt')
-    cl.add_argument('-f', '--fpt',
-                    help='Fingerprint output file (default=STDOUT, auto=calculate from xios)',
-                    default='auto')
-    cl.add_argument('-o', '--out',
-                    help='Output file name (default=%(default)s)',
-                    default='distance.out')
-
-
+    cl.add_argument('-r', '--fpt ',
+                    help='filename for fingerprint files, can be wildcard  (default=%(default)s',
+                    default='.fpt')
+    cl.add_argument('-o', '--distance',
+                    help='Distance output file name (default=%(default)s, auto=calculate from fpt)',
+                    default='fingerprint.dist')
+    cl.add_argument('-m', '--outdir',
+                    help='Directory for output distance files (default=%(default)s)',
+                    default='./')
     args = cl.parse_args()
 
-    if str(args.fpt) == 'auto':
-        args.fpt = fpt_from_xios(args)
+    # if output filename is auto, create a file name from the fingerprint file name
+    if str(args.distance) == 'auto':
+        out = args.distance
+        out = out.replace('*', '_all')
+        args.distance = out
 
     return args
+
+
 # --------------------------------------------------------------------------------------------------
 # main
 # --------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
     daytime = datetime.datetime.now()
 
+    # setup directories and suffix according to command line
     opt = process_command_line()
-    fpt_dir = opt.fpt'
+    fpt_dir = opt.fpt
     fpt_suffix = opt.suffix
     outfile = opt.out
 
@@ -60,20 +72,13 @@ if __name__ == '__main__':
     print(f'{fpt_n} fingerprints read')
 
     # distance calculation
-    # for i in range(fpt_n):
-    #     fpt1 = fpt[i].information['File']
-    #     for j in range(i + 1, fpt_n):
-    #         fpt2 = fpt[j].information['File']
-    #         j = fpt.jaccard_sim([i,j])
-    #         bc = fpt.bray_curtis_dis([i,j])
-    #         print(f'{fpt1}\t{fpt2}\t{j[2]:.3f}\t{bc:[2]:.3f}')
-    maximum = {'jaccard':0, 'bray-curtis':0}
+    maximum = {'jaccard': 0, 'bray-curtis': 0}
     minimum = {'jaccard': 1000000, 'bray-curtis': 1000000}
-    
+
     jaccard = fpt.jaccard_sim([])
     bc = fpt.bray_curtis_dis([])
 
-    out = open( outfile, 'w')
+    out = open(outfile, 'w')
     for d in jaccard:
         i = d[0]
         j = d[1]
@@ -81,7 +86,7 @@ if __name__ == '__main__':
         out.write(f'{basename(fpt[j].information["File"])}\t')
         out.write(f'{d[2]:.3f}\t')
         out.write(f'{bc[j][2]:.3f}\n')
-        
+
         maximum['jaccard'] = max(maximum['jaccard'], d[2])
         minimum['jaccard'] = min(minimum['jaccard'], d[2])
         maximum['bray-curtis'] = max(maximum['bray-curtis'], bc[j][2])
