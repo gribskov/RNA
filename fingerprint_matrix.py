7924c0dc6c581f32501ad0b4ca33c33809c582cd
mgribsko <mgribskov@gmail.com>
2023-02-28 11:10:19 -0500

    fingerprint_matrix - added motif selection and tested OK

--- fingerprint_matrix.py
+++ fingerprint_matrix.py
@@ -1,5 +1,5 @@
 """=================================================================================================
-Convert a set of fingerprint files into a binary form for rapid reading and manipultation
+Convert a set of fingerprint files into a binary form for rapid reading and manipulation
 ================================================================================================="""
 from fingerprint import FingerprintMatrix
 
@@ -8,5 +8,8 @@ if __name__ == '__main__':
     selection = 'data/fpt/*'
     fmat = FingerprintMatrix()
     fmat.read_files(selection)
+    fmat.select_min_max(2, 2)
+    fmat.select_min_max(0, 7, setval=True, recalculate=True)
+    # fmat.index2matrix()
 
     exit(0)
