1af7aad89bfae0a5ac5773399fe64b7f35ae84bd
Michael Gribskov <mgribsko@gmail.com>
2023-03-02 07:26:49 -0500

    fingerprint_matrix works on test data with detection of end condition

--- fingerprint_matrix.py
+++ fingerprint_matrix.py
@@ -46,34 +46,93 @@ class Kmeans():
 
         return len(self.data)
 
-    def cluster(self):
+    def cluster(self, delta=0.1):
 
         # get cluster centroids
+        group = self.group
+        centroid = self.centroid
         nfeature = len(self.data[0])
-        for group in self.group:
-            self.centroid.append( [0 for _ in range(nfeature)])
-            for i in group:
-                self.centroid[-1] += self.data[i]
-            self.centroid[-1] /= len(group)
+        npoint = len(self.data)
+        ngroup = len(self.group)
+        maxval = 0
+        error = 0
+        error_old = 0
+        cycle = 0
 
-        for g in range(len(self.group)):
-            for point in self.group[g]:
-                dist = sum(abs(self.data[point] - self.centroid[g]))
+        centroid = [[0 for _ in range(nfeature)] for _ in range(ngroup)]
+        newgroup = [[] for _ in range(ngroup)]
+
+        while True:
+            cycle += 1
+
+            # calculate centroid positions
+            for g in range(ngroup):
+                centroid[g][:] = [0 for g in range(nfeature)]
+                for i in group[g]:
+                    centroid[g] += self.data[i]
+                    maxval = max(maxval, sum(self.data[i]))
+
+                if len(group[g]):
+                    centroid[g] /= len(group[g])
+
+            error = 0
+            for point in range(npoint):
+                # for each point find the distance to each centroid and reassign
+                mindist = maxval
+                minid = ngroup + 1
+                for g in range(ngroup):
+                    # each current group centroid
+                    dist = sum(abs(self.data[point] - centroid[g]))
+                    if dist < mindist:
+                        mindist = dist
+                        minid = g
+                    if point in group[g]:
+                        # add the distance to the current assigned centroid to
+                        # the error
+                        error += dist
+
+                newgroup[minid].append(point)
+
+            # current error
+            error /= npoint
+            print(f'cycle={cycle}\t error={error:.1f}')
+            for g in range(ngroup):
+                print(f'\t{g}\t{group[g]}')
+
+            group = newgroup[:]
+            for g in range(ngroup):
+                newgroup[g] = []
+
+            if (abs(error_old - error) / error < delta):
+                break
+
+            error_old = error
+
+        self.centroid = centroid
+        self.group = group
 
         return
 
+
 # main program
 if __name__ == '__main__':
-    selection = 'data/fpt/*'
+    selection = 'data/fpt/oldfpt/*.xpt'
     fmat = FingerprintMatrix()
     fmat.read_files(selection)
     # fmat.select_min_max(2, 2)
     # fmat.select_min_max(0, 7, setval=True, recalculate=True)
     # fmat.index2matrix()
-    k = Kmeans(2)
+    k = Kmeans(5)
     ndata = k.assign_data(fmat.fpt)
     print(f'{ndata} points read for k={k.k}')
 
     k.cluster()
+    g = 0
+    fptname = list(fmat.fpt_id.keys())
+    for group in k.group:
+        g += 1
+        print(f'group {g}')
+        for f in group:
+            print(f'\t{fptname[f]}')
 
     exit(0)
