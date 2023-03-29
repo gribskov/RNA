88641d4591617392be02022fc7a712ab01d7b9ea
mgribsko <mgribskov@gmail.com>
2023-03-29 10:46:39 -0400

    fingerprint_compare - in process of adding command line

--- fingerprint_compare.py
+++ fingerprint_compare.py
@@ -6,11 +6,75 @@ Michael Gribskov     20 September 2022
 import glob
 import os
 import sys
-
+import argparse
 from lxml import etree
-
 from fingerprint import Fingerprint
 
+
+def process_command_line():
+    """---------------------------------------------------------------------------------------------
+
+    :return: namespace      command line arguments
+    ---------------------------------------------------------------------------------------------"""
+    cl = argparse.ArgumentParser(
+        description='Compare sets of XIOS fingerprints',
+        formatter_class=lambda prog: argparse.HelpFormatter(prog, width=120, max_help_position=40)
+    )
+    cl.add_argument('-n', '--new',
+                    help='New format fingerprint (default%(default))',
+                    action='append', nargs='*',
+                    default='*.fpt')
+    cl.add_argument('-h', '--hex',
+                    help='Old hexadecimal encoded format fingerprint (default%(default))',
+                    action='append', nargs='*',
+                    default='*.xpt')
+    cl.add_argument('-o', '--outputdir',
+                    help='Directory for fingerprint output (default=%(default)s)',
+                    default='./')
+
+    args = cl.parse_args()
+
+    return args
+
+def read_new_fpt(target_list):
+    """---------------------------------------------------------------------------------------------
+    Read a set of fingerprints. Each glob from the filename is put in a separate set of motif names
+    in the string format
+
+    in the new format (2022-2023, yaml file0 such as
+    - fingerprint:
+    - information:
+          Date: '2022-05-06 09:27:07'
+          File: ./fpt/rnasep_a4.Pseudoanabaena_sp.PCC6903.w4.d5.fpt
+          Motif database: ../RNA/data/2to7stem.mdb.pkl
+          RNA structure: ./xios/rnasep_a4.Pseudoanabaena_sp.PCC6903.w4.d5.xios
+      - total: 50001
+      - nmotif: 909
+      - motif:
+          0i1.0i2.0i3.0o4.: 981
+          0i1.1o2.2j0.0i3.0i4.: 877
+          ...
+
+    :param target_list: list    string with paths to sets of files to compare
+    :return: dict               sets of motifs; set is a list of strings such as 0i1.0i2.0i3.0o4
+    ---------------------------------------------------------------------------------------------"""
+    motif_set = {}
+    for target in target_list:
+        fptfilelist = fastafiles = glob.glob(target)
+        set = {}
+
+        n = 0
+        for fptfile in fptfilelist:
+            n += 1
+            id = os.path.basename(fptfile)
+
+            # read new fingerprint as YAML
+            fpt = Fingerprint()
+            fpt.readYAML(fptfile)
+            set[id] = fpt.motif
+
+        motif_set[target] = set
+        return motif_set
 
 def decodedfs(hexstr):
     """---------------------------------------------------------------------------------------------
@@ -45,30 +109,29 @@ def decodedfs(hexstr):
 # main program
 # --------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
-    # get old new fingerprint name amd derive old fingerprint name
+    daytime = datetime.datetime.now()
+    runstart = daytime.strftime('%Y-%m-%d %H:%M:%S')
+    opt = process_command_line()
 
-    # olddir = 'data/fpt/oldfpt/'
-    # newdir = 'data/fpt/'
-    olddir = sys.argv[1]
-    newdir = sys.argv[2]
-    target = newdir + '*.fpt'
+    print(f'new format fingerprints: {}')
+
 
     print(f'fingerprint directory: {newdir}')
     print(f'target: {olddir}')
 
     fptfilelist = fastafiles = glob.glob(target)
-    #print(fptfilelist)
-    #print()
+    # print(fptfilelist)
+    # print()
 
     n = 0
     comp = {}
     for newfile in fptfilelist:
         oldfile = os.path.basename(newfile)
-        id = oldfile.replace('.fpt','')
+        id = oldfile.replace('.fpt', '')
         comp[id] = {}
         n += 1
         print(f'{n:4d}\t{id}')
-        #print(comp)
+        # print(comp)
 
         # read new fingerprint as YAML
         fpt = Fingerprint()
@@ -93,14 +156,14 @@ if __name__ == '__main__':
         old_list = []
         for code in oldlist:
             if code not in fpt.motif:
-               old_miss += 1
-               old_list.append(code)
+                old_miss += 1
+                old_list.append(code)
 
         comp[id]['old_miss'] = old_miss
         comp[id]['old_list'] = old_list
 
         # see if new motifs exist in old
-        new_miss =  0
+        new_miss = 0
         new_list = []
         for code in fpt.motif:
             if code not in oldlist:
@@ -108,15 +171,15 @@ if __name__ == '__main__':
                 new_list.append(code)
 
         comp[id]['new_miss'] = new_miss
-        #comp[id]['new_list'] = new_list
-
+        # comp[id]['new_list'] = new_list
 
     line = 0
-    for fpt in sorted(comp, key=lambda f: ((comp[f]['count_old']-comp[f]['count_new']),f)):
+    for fpt in sorted(comp, key=lambda f: ((comp[f]['count_old'] - comp[f]['count_new']), f)):
         line += 1
-        #print(fpt, comp[fpt])
+        # print(fpt, comp[fpt])
         f = comp[fpt]
-        print(f"{line:4d}\t{f['count_old']}\t{f['count_new']}\t{f['old_miss']}\t{f['new_miss']}\t{fpt}")
+        print(
+            f"{line:4d}\t{f['count_old']}\t{f['count_new']}\t{f['old_miss']}\t{f['new_miss']}\t{fpt}")
         if f['old_list']:
             for motif in f['old_list']:
                 print(f'\t\t\t\t\t{motif}')
