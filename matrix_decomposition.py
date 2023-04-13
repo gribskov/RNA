aa2f9055d9f06cd86e1c7b78977e413cafd388d8
Michael Gribskov <mgribsko@gmail.com>
2023-04-13 16:02:30 -0400

    matrix_decomposition - read data from pickled version, pca and plot, working version

--- matrix_decomposition.py
+++ matrix_decomposition.py
@@ -7,6 +7,8 @@ import matplotlib.pyplot as plt
 from sklearn import decomposition
 from sklearn import datasets
 
+from fingerprint import FingerprintMatrix
+import os
 
 if __name__ == '__main__':
 
@@ -44,38 +46,47 @@ if __name__ == '__main__':
     # ax.zaxis.set_ticklabels([])
     #
     # plt.show()
-    file = 'fmatrix.tsv'
-    # motif = pd.read_csv(file, sep='\t', index_col=0)
-    # motif = np.loadtxt(file, dtype=int, usecols=(1,2,3,4,5,6,7,8), skiprows=1)
-    motif = np.loadtxt(file, dtype=str, usecols=(1,2,3,4,5,6,7,8))
-    colnames = motif[0,:]
-    motif = np.loadtxt(file, dtype=str, skiprows=1)
-    rownames = motif[:,0]
-    values = np.transpose(motif[:,1:9].astype(np.int32))
 
-    # print(motif.head())
-    # print(motif.columns)
-    # print(motif.info())
+    # file = 'fmatrix.tsv'
+    # read from pickled fingerprint matrix with fingerprints = rows and motifs = columns
+    fmat = FingerprintMatrix.unpickle('fmatrix.pkl')
+    motif_mincount = 10
+    motif_maxcount = 140
+    fmat.select_min_max(motif_mincount, motif_maxcount, False, recalculate=True)
+    f = fmat.fpt
+    motif = np.array([[f[row][col] for col in range(len(f[0]))] for row in range(len(f))])
+    colnames = fmat.motifs_selected()
+
+    # get fingerprint names and trim
+    rownamedict = fmat.fpt_id
+    rownames = []
+    for name in rownamedict:
+        name = name.replace('.fpt','')
+        name = name.replace('.xios.out', '')
+        name = os.path.basename(name)
+        rownames.append(name)
+
     # groups/labels
+    # for the curated data, if the first token after splitting on . and _ matches, it is the same group
+    groups = {}
+    gindex = []
     g = []
-    for col in colnames:
-        print(col)
-        if col.find('5S')>-1:
-            g.append(0)
-        elif col.find('tmRNA')>-1:
-            g.append(1)
-        elif col.find('rnasep')>-1:
-            g.append(2)
+    for name in rownames:
+        token = name.split('.')
+        token = token[0].split('_')[0]
+        if token not in groups:
+            groups[token] = len(groups)
+            gindex.append(token)
 
-
+        g.append(groups[token])
 
     pca = decomposition.PCA(n_components=3)
-    pca.fit(values)
-    X = pca.transform(values)
+    pca.fit(motif)
+    X = pca.transform(motif)
     components = pca.components_
     for c in pca.components_:
         count = 0
-        for i in sorted(range(len(c)), key=lambda i:c[i], reverse=True):
+        for i in sorted(range(len(c)), key=lambda i: c[i], reverse=True):
             print(f'{i}\t{c[i]:.3g}\t{rownames[i]}')
             count += 1
             if count > 5:
@@ -89,8 +100,22 @@ if __name__ == '__main__':
     ax = fig.add_subplot(111, projection="3d", elev=48, azim=134)
     ax.set_position([0, 0, 0.95, 1])
     # y = np.choose(y, [1, 2, 0]).astype(float)
-    ax.scatter(X[:, 0], X[:, 1], X[:, 2], c=g, cmap=plt.cm.nipy_spectral, edgecolor="k")
+    # scatter = ax.scatter(X[:, 0], X[:, 1], X[:, 2], c=g, cmap=plt.cm.nipy_spectral, label=g, edgecolor="k")
+    scatter = ax.scatter(X[:, 0], X[:, 1], X[:, 2], c=g, cmap=plt.cm.Accent, label=g, edgecolor="k", s=50, alpha = 0.8)
+    a = scatter.legend_elements()
+
+    for i in range(len(a[1])):
+        a[1][i] = gindex[i]
+
+    legend1 = ax.legend(*a,
+                        loc="upper left", title="Groups")
+    # gmean = [0 for _ in g]
+    # for name, label in groups.items():
+    #    gmean[]
+    # ax.text3D(X[g==label, 0].mean(),
+    #     name,
+    #     horizontalalignment="center",
+    #     bbox=dict(alpha=0.5, edgecolor="w", facecolor="w"),
+    # )
     plt.show()
     exit(0)
-
-
