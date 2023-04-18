b35edd592e4d3b4227238ad407aea987946af7b2
mgribsko <mgribskov@gmail.com>
2023-04-18 11:39:30 -0400

    manager_new - construct command from workflow

--- manager_new/manager.py
+++ manager_new/manager.py
@@ -1,20 +1,68 @@
 import yaml
 
 
-def yaml_read(filename):
-    """---------------------------------------------------------------------------------------------
-    read the workflow description from the yaml file
+class Workflow:
 
-    :param filename: string     yaml workflow description
-    :return: dict               workflow as a python dictionary
-    ---------------------------------------------------------------------------------------------"""
-    fp = open(filename, 'r')
-    workflow = yaml.load(fp, Loader=yaml.FullLoader)
-    fp.close()
-    return workflow
+    def __init__(self):
+        """-----------------------------------------------------------------------------------------
 
+        -----------------------------------------------------------------------------------------"""
+        self.plan = {}
+        self.command = ''
+
+    def yaml_read(self, filename):
+        """-----------------------------------------------------------------------------------------
+        read the workflow description from the yaml file
+
+        :param filename: string     yaml workflow description
+        :return: dict               workflow as a python dictionary
+        -----------------------------------------------------------------------------------------"""
+        fp = open(filename, 'r')
+        self.plan = yaml.load(fp, Loader=yaml.FullLoader)
+        fp.close()
+        return self.plan
+
+    def command_generate(self, stage):
+        """-----------------------------------------------------------------------------------------
+        generate a list of commands from the workflow for a specific stage
+        1. split the command into tokens
+        2. for each token, match to the other keywords in plan['stage'][stage] and replace
+           %keyword with the value from the yaml. because % substitutions are processed first, they
+           can include global $ definitions
+        3. for each token, match to keywords in plan['definitions'] and replace $keyword with the
+           value from th yaml
+        4. options in <> are processed using plan['stage'][stage]['rule'] at runtime
+
+        :param stage: string            stage in workflow
+        :return: string                 command string
+        -----------------------------------------------------------------------------------------"""
+        current = self.plan['stage'][stage]
+
+        command = current['command']
+        token = command.split()
+        # process %
+        for t in range(len(token)):
+            for d in current:
+                if d == 'command':
+                    continue
+                target = f'%{d}'
+                if token[t].find(target) > -1:
+                    token[t] = token[t].replace(target, current[d])
+
+        # process $
+        for t in range(len(token)):
+            for d in self.plan['definitions']:
+                target = f'${d}'
+                if token[t].find(target) > -1:
+                    token[t] = token[t].replace(target,self.plan['definitions'][d])
+
+        new = ' '.join(token)
+        self.command = new
+        return new
 
 if __name__ == '__main__':
-    workflow = yaml_read('workflow.yaml')
+    w = Workflow()
+    w.yaml_read('workflow.yaml')
 
+    commands = w.command_generate('xios')
     exit(0)
