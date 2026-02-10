f82e4ac2f19272dea40793e53d2c714e1173ad6a
Michael Gribskov <mgribsko@gmail.com>
2026-02-10 14:17:27 -0500

    test_fingerprint_pipeline.sh - added fingerprint to pipeline

--- scripts/test_fingerprint_pipeline.sh
+++ scripts/test_fingerprint_pipeline.sh
@@ -5,6 +5,9 @@
 # RNAstructure/partition
 # RNAstructure/stochastic
 # RNA/stochastic_to_xios
+#
+# usage:
+# bash test.sh ../../standards/fasta_fixed/rnasep_a2.Alcaligenes_eutrophus.fa
 #-------------------------------------------------------------------------------
 export DATAPATH=../../RNAstructure/data_tables/
 rs_exe=../../RNAstructure/exe
@@ -21,7 +24,7 @@ pfs=${pfs/.fa/.pfs}
 echo "partition function save file: $pfs"
 partition_command="$rs_exe/partition $f $pfs"
 echo $partition_command
-#$partition_command
+$partition_command
 
 # stochastic
 echo -e "\nStochastic"
@@ -29,12 +32,19 @@ ct=${pfs/.pfs/.ct}
 echo "ct: $ct"
 stochastic_command="$rs_exe/stochastic $pfs $ct"
 echo $stochastic_command
-#$stochastic_command
+$stochastic_command
 
 # stochastic_to_xios
 echo -e "\nstochastic_to_xios"
 xios=${ct/.ct/.xios}
 source activate   /home/mgribsko/.conda/envs/cent7/5.3.1-py37/rna
-command="python ../../RNA/stochastic_to_xios.py $ct $xios"
-echo $command
-$command
+s2rcommand="python ../../RNA/stochastic_to_xios.py $ct $xios"
+echo $s2rcommand
+$s2rcommand
+
+# fingerprint_random
+echo -e "\nfingerprint_random"
+fpt=${xios/.xios/.fpt}
+fingerprint_command="python ../../RNA/fingerprint_random.py -m ../../RNA/data/2to7stem.mdb.pkl -r $xios -f $fpt"
+echo $fingerprint_command
+$fingerprint_command
