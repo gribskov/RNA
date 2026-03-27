364c17f6c6761a1306d2ba3d89cd5c66daaf82b5
Michael Gribskov <mgribsko@gmail.com>
2026-03-27 13:40:14 -0400

    fingerprint_compare_curated.py - updateing to run on a directory of directories as in parameter sweep

--- fingerprint_compare_curated.py
+++ fingerprint_compare_curated.py
@@ -11,7 +11,8 @@ from fingerprint import FingerprintSet, Fingerprint
 
 if __name__ == '__main__':
     curdir = f'{sys.argv[1]}/*.fpt'
-    testdir = f'{sys.argv[2]}/*.fpt'
+    testparent = f'{sys.argv[2]}'
+
 
     # read curated fingerprints
     fpt_list = glob.glob(curdir)
@@ -38,38 +39,83 @@ if __name__ == '__main__':
         count += 1
         print(f'\t{count}\t{name}')
 
-    print(f'{len(test)} test fingerprints read from {testdir}')
+    print(f'{len(test)} test fingerprints read from {testparent}')
+    # all directories in testparent
+    directories = [entry.name for entry in os.scandir(testparent) if entry.is_dir()]
+    dirlist = []
+    fptlist = {}
+    dcount = 0
+    fptcount = 0
+    for dir in directories:
+        fpt = dir + '/fpt'
+        dcount += 1
+        with os.scandir(fpt) as entries:
+            for entry in entries:
+                if entry.name.endswith('.fpt'):
+                    # each file in the fpt directory
+                    fptcount += 1
+                    try:
+                        c = curated[fpt]
+                        t = test[fpt]
+                    except KeyError as e:
+                        print(f'!Error: {fpt} not found in test')
+                        continue
+                    clist = []
+                    tlist = []
+                    both = []
+                    for m in c.motif:
+                        if m in t.motif:
+                            both.append(m)
+                        else:
+                            clist.append(m)
+                    for m in t.motif:
+                        if m not in c.motif:
+                            tlist.append(m)
 
-    for fpt in sorted(curated):
-        try:
-            c = curated[fpt]
-            t = test[fpt]
-        except KeyError as e:
-            print(f'{fpt} not found in test')
-            continue
-        clist = []
-        tlist = []
-        both = []
-        for  m in c.motif:
-            if m in t.motif:
-                both.append(m)
-            else:
-                clist.append(m)
-        for m in t.motif:
-            if m not in c.motif:
-                tlist.append(m)
+                    nb = len(both)
+                    nc = len(clist)
+                    nt = len(tlist)
+                    recall = nb / (nb + nc)
+                    precision = nb / (nb + nt)
+                    try:
+                        jaccard = nb / (nt + nc + nb)
+                    except ZeroDivisionError:
+                        jaccard = 0
+                    print(f'{recall:6.3f}  {precision:6.3f} {jaccard:6.3f}  ', end='')
+                    print(f'{len(clist)}\t{len(both)}\t{len(tlist)}\t{fpt}')
 
-        nb = len(both)
-        nc = len(clist)
-        nt = len(tlist)
-        recall = nb / (nb + nc)
-        precision = nb / (nb + nt)
-        try:
-            jaccard = nb / (nt + nc + nb)
-        except ZeroDivisionError:
-            jaccard = 0
-        print(f'{recall:6.3f}  {precision:6.3f} {jaccard:6.3f}  ', end='')
-        print(f'{len(clist)}\t{len(both)}\t{len(tlist)}\t{fpt}')
+
+            fptlist[dir] = fptcount
+    # for fpt in sorted(curated):
+    #     try:
+    #         c = curated[fpt]
+    #         t = test[fpt]
+    #     except KeyError as e:
+    #         print(f'{fpt} not found in test')
+    #         continue
+    #     clist = []
+    #     tlist = []
+    #     both = []
+    #     for  m in c.motif:
+    #         if m in t.motif:
+    #             both.append(m)
+    #         else:
+    #             clist.append(m)
+    #     for m in t.motif:
+    #         if m not in c.motif:
+    #             tlist.append(m)
+    #
+    #     nb = len(both)
+    #     nc = len(clist)
+    #     nt = len(tlist)
+    #     recall = nb / (nb + nc)
+    #     precision = nb / (nb + nt)
+    #     try:
+    #         jaccard = nb / (nt + nc + nb)
+    #     except ZeroDivisionError:
+    #         jaccard = 0
+    #     print(f'{recall:6.3f}  {precision:6.3f} {jaccard:6.3f}  ', end='')
+    #     print(f'{len(clist)}\t{len(both)}\t{len(tlist)}\t{fpt}')
 
 
     exit(0)
