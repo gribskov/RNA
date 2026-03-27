2d5fc997da4d1c8dbd179042ad6b55f71a2c2c1d
Michael Gribskov <mgribsko@gmail.com>
2026-03-27 07:23:19 -0400

    check_files.py - getting list of original sequence files

--- manager_new/check_files.py
+++ manager_new/check_files.py
@@ -12,9 +12,26 @@ import os
 # main
 # ======================================================================================================================
 if __name__ == '__main__':
+    # get a list of input files
+    indata = '../../RNA/data/fasta_fixed/*.fa'
+    with os.scandir(indata) as entries:
+        n = 0
+        for entry in entries:
+            if entry.name.endswith('fa'):
+                n += 1
+                print(f'{n}\t{entry.name}')
+
+
+
     directories = [entry.name for entry in os.scandir('.') if entry.is_dir()]
 
     for dir in directories:
         print(dir)
+        fpt = dir + '/fpt'
+
+        with os.scandir('.') as entries:
+            for entry in entries:
+                if entry.is_file():
+                    print(f"{entry.name}: {entry.stat().st_size} bytes")
 
     exit(0)
