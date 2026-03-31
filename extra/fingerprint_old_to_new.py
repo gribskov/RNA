fa7154d111289c6aa0d64728e3e72902d6a62ba1
gribskov <gribskov@purdue.edu>
2026-03-31 06:18:39 -0400

    fingerprint_old_to_new.py - removed directory from input file when constructing output

--- extra/fingerprint_old_to_new.py
+++ extra/fingerprint_old_to_new.py
@@ -9,21 +9,21 @@ usage:
 2026-03-30 gribskov
 ====================================================================================================================="""
 import sys
+import os
 import xios
 import topology
 from fingerprint import Fingerprint
+
 # ======================================================================================================================
 # Main
 # ======================================================================================================================
 if __name__ == '__main__':
-
-
     xptfile = sys.argv[1]
     xpt = Fingerprint()
     xpt.readXML(xptfile)
 
     xpt.information['fingerprint']['converted from'] = f'{xptfile.rstrip()} at: {xpt.setdate()}'
-    fptfile = xptfile.replace('.xpt', '.fpt')
+    fptfile = os.path.basename(xptfile).replace('.xpt', '.fpt')
     xpt.writeYAML(fptfile)
 
     exit(0)
